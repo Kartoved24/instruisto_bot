ca78ee762070176f4841c5b83a68c3215e559c32
KarToveD <67981770+Kartoved24@users.noreply.github.com>
2023-01-25 13:38:54 +0300

    Первые функциональные добавления
    - добавлены инлайн кнопки ведущие на словарь Кондратьева и чат эсперантистов
    -  добавлена возможность связаться с разработчиком
    - переработано окно приветствия бота

--- instruisto.py
+++ instruisto.py
@@ -1,17 +1,32 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 from config import TOKEN_API
 from random import random
 
-HELP_COMMAND  = """
+HELP_COMMAND = """Привет, эсперантист! Я бот-преподаватель языка <strong>Эсперанто</strong>. Я помогу тебе выучить этот прекрасный язык, расскажу много интересного и полезного об Эсперанто. Ознакомься с моими командами ниже:
+
 <strong>/help</strong> - <em>список команд</em>
 <strong>/start</strong> - <em>запустить бота</em>
-<strong>/contact</strong> - <em>связаться с разработчиком</em>"""
+<strong>/contact</strong> - <em>связаться с разработчиком</em>
+<strong>/links</strong> - <em>полезные ссылки</em>"""
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
-kb = ReplyKeyboardMarkup()
-kb.add(KeyboardButton('/help'))
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+kb.add(KeyboardButton('/help')).add(KeyboardButton('/start')).add(KeyboardButton('/contact')).add(KeyboardButton('/links'))
+
+ikb = InlineKeyboardMarkup(row_width=2)
+ikb1 = InlineKeyboardButton(text='Связаться с разработчиком',
+                            url='https://t.me/kartoved')
+ikb.add(ikb1)
+
+ikb_links = InlineKeyboardMarkup(row_width=1)
+dictionary_button = InlineKeyboardButton(text='Словарь Кондратьева',
+                            url='rueo.ru')
+chat_button = InlineKeyboardButton(text='Чат эсперантистов в Telegram',
+                            url='https://t.me/Esperantujoo')
+ikb_links.add(dictionary_button, chat_button)
 
 
 @dp.message_handler(commands=['help'])
@@ -21,12 +36,17 @@ async def help(message: types.Message):
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    pass
+    await message.answer(text=HELP_COMMAND, parse_mode='HTML', reply_markup=kb)
 
 @dp.message_handler(commands=['contact'])
 async def contact(message: types.Message):
-    pass
+    await message.answer(text='Пиши мне про баги и слова благодарности 🥳',
+                         reply_markup=ikb)
 
+@dp.message_handler(commands=['links'])
+async def get_useful_links(message: types.Message):
+    await message.answer(text='Полезные ссылки',
+                         reply_markup=ikb_links)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import TOKEN_API
 from random import random
 
@@ -8,12 +10,15 @@ HELP_COMMAND = """
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
+ikb = InlineKeyboardMarkup(row_width=2)
+ikb1 = InlineKeyboardButton(text='bla',
+                            url='ya.ru')
+ikb.add(ikb1)
 
-
-@dp.message_handler(commands=['give'])
-async def give_cat(message: types.Message):
-    await message.reply(text='Лови малыша Грогу!')
-    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEHaBJjzyGJyPHvKfo5XCejPoL92CHOYAACeAIAAladvQr8ugi1kX0cDC0E')
+@dp.message_handler(commands=['links'])
+async def send_links(message: types.Message):
+    await bot.send_message(text='Ссылки',
+                           reply_markup=ikb)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
